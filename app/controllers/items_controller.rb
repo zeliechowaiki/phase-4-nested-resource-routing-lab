f0b4b9e3ff40c93a3f05e7c45655a71576171e15
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-06 10:04:24 -0400

    commit tree 783103d4b7c35c8ff85100b1563ac35a40198176

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response(invalid)
+    render json: { errors: invalid.message }, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:show, :create, :index]
+  end
 end
